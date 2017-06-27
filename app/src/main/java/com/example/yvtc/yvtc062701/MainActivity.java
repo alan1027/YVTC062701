fbedd2ac0302c5feca778f724d68150d775b0286
Shaojung <shaojung@gmail.com>
2017-06-27 13:41:11 +0800

    use intent extra to put value.

--- app/src/main/java/com/example/yvtc/yvtc062701/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc062701/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +18,13 @@ public class MainActivity extends AppCompatActivity {
     public void click1(View v)
     {
         Intent it = new Intent(MainActivity.this, SecActivity.class);
+        EditText ed1, ed2;
+        ed1 = (EditText) findViewById(R.id.editText2);
+        ed2 = (EditText) findViewById(R.id.editText3);
+        int a = Integer.valueOf(ed1.getText().toString());
+        int b = Integer.valueOf(ed2.getText().toString());
+        it.putExtra("va", a);
+        it.putExtra("vb", b);
         startActivity(it);
     }
 }
--- app/src/main/java/com/example/yvtc/yvtc062701/SecActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc062701/SecActivity.java
@@ -1,7 +1,9 @@
 package com.example.yvtc.yvtc062701;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class SecActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sec);
+
+        Intent it = getIntent();
+        int a = it.getIntExtra("va", 0);
+        int b = it.getIntExtra("vb", 0);
+        TextView tv = (TextView) findViewById(R.id.textView);
+        tv.setText(String.valueOf(a + b));
+
     }
 }
